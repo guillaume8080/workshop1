b97d3e92f1dbf4b313eba98a8f872c68d59e5e3b
guillaume8080 <guillaume.gouy@ynov.com>
2022-11-11 15:17:09 +0100

    logique réponse faux ou vrai ok

--- jeu-w1/api/controllers/JeuController.js
+++ jeu-w1/api/controllers/JeuController.js
@@ -13,8 +13,10 @@ module.exports = {
 
   testJsonDB: async function  (req, res) {
 
-
-    // var axios = require('axios');
+    const idquizz = 1;
+    const score = 0;
+    const responses = ["ceci est une connerie", "ceci est une connerie","ceci est une connerie","ceci est une connerie"];
+    const idTrueResponse = 1;
 
 
     var db = new JsonDB(new Config("mybase.json", false, true, '/'));
@@ -22,15 +24,14 @@ module.exports = {
     // await db.push("/test1","super test");
     // await db.delete("/");
 
-    // await db.push("0" ,quizzes , false );
-    // await db.save();
     var data = await db.getData("/");
     console.log(data);
 
     console.log(data.quizz[0].question);
 
-
-  res.redirect("/jeu1");
+    // return  res.view('pages/event/createAddress' , {leDebut: debut, leLibelle:libelle , laFin:fin , collectionAdresses:labelsReturned });
+    return res.view('pages/jeu1' , { leScore: score , lesReponses: responses , idVraiReponse: idTrueResponse});
+    // res.redirect("/jeu1");
 
 
   }
